6c91c2aeb7e9a538ef17865d72fbd8d4e84c8b72
Lin Cheng Wei <snoopy90338@gmail.com>
2020-12-20 23:02:59 +0800

    Add SQL template.

---
+++ template.sql
@@ -0,0 +1,14 @@
+-- Q1: What planets did Darth Vader visit in movie 2?
+-- Write your answer for Q1 over here.
+
+-- Q2: Find all characters that never visited any rebels planets.
+-- Write your answer for Q2 over here.
+
+-- Q3: Who visited planets of his/her affiliation in movie 1?
+-- Write your answer for Q3 over here.
+
+-- Q4: Which planet(s) has been visited by more than three different characters?
+-- Write your answer for Q4 over here.
+
+-- Q5: For each movie, which character(s) visited the highest number of planets?
+-- Write your answer for Q5 over here.
